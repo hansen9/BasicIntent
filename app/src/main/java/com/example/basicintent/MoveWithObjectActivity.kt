a3f9feda55b3f57fae42ae3f4330b1c9ddcc1ec9
Hansen <tofvoid@gmail.com>
2022-03-13 16:24:23 +0700

    initial commit

---
+++ app/src/main/java/com/example/basicintent/MoveWithObjectActivity.kt
@@ -0,0 +1,22 @@
+package com.example.basicintent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MoveWithObjectActivity : AppCompatActivity() {
+    companion object{
+        const val EXTRA_PERSON = "extra_person"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_object)
+
+        val tvObject:TextView = findViewById(R.id.tv_object_received)
+        val person = intent.getParcelableExtra<Person>(EXTRA_PERSON) as Person
+
+        val text = "name : ${person.name.toString()}, \nemail : ${person.email},\nage : ${person.age},\nlocation : ${person.city}"
+        tvObject.text = text
+    }
+}
